30d5f19768fe5fac2e1fda90cbac1ee6a22558be
PrashantSBasnet <50170332+PrashantSBasnet@users.noreply.github.com>
2021-11-17 12:42:55 +0545

    Step 1
    Includes serviceregistry, user microservice and department microservice

---
+++ microservices/micro-department/src/test/java/com/example/microdepartment/MicroDepartmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microdepartment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroDepartmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
